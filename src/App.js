0e547af4a9c24279b08955b3028c6451f7c243ab
Jikku Jose <jikkujose@gmail.com>
2018-07-15 17:52:35 +0530

    Implement card like in HackClub

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import "./App.css"
 
 import { Card } from "./Card"
 
+const zach = {
+  color: "red",
+  name: "Zach Latta",
+  image: "https://hackclub.com/team/zach.jpg",
+  role: "Executive Director",
+  description:
+    "Zach dropped out of high school after his freshman year to work in the technology industry and had over 5 million people using his software by the time he turned 17. He founded Hack Club to build the program he wish he had in high school and has been awarded the Thiel Fellowship and Forbes 30 Under 30 for his work.",
+}
+
 class App extends Component {
   render() {
     return (
       <div className="p-32 min-h-screen">
-        <Card color="indigo" />
+        <Card {...zach} />
       </div>
     )
   }
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export function Card({ color = "yellow" }) {
+export function Card({ name, color, image, description, role }) {
   return (
     <div
       className={`container bg-${color}-lightest rounded-lg p-5 flex w-64`}
@@ -8,7 +8,7 @@ export function Card({ color = "yellow" }) {
     >
       <div className="image">
         <img
-          src="https://hackclub.com/team/zach.jpg"
+          src={image}
           className="rounded-full"
           style={{ width: "20rem" }}
           alt="Zach profile pic"
@@ -16,20 +16,16 @@ export function Card({ color = "yellow" }) {
       </div>
       <div className="text-container ml-3 font-sans">
         <div className="heading flex mb-1" style={{ alignItems: "center" }}>
-          <div className="title font-bold text-lg mr-2">Zach Latta</div>
+          <div className="title font-bold text-lg mr-2">{name}</div>
           <div
-            className={`label bg-${color} text-white py-1 px-2 rounded text-sm`}
+            className={`label bg-${color} text-white py-1 px-2 rounded text-3`}
             style={{ textTransform: "uppercase" }}
           >
-            Executive Director
+            {role}
           </div>
         </div>
         <div className="description leading-normal text-grey-darker text-sm">
-          Zach dropped out of high school after his freshman year to work in the
-          technology industry and had over 5 million people using his software
-          by the time he turned 17. He founded Hack Club to build the program he
-          wish he had in high school and has been awarded the Thiel Fellowship
-          and Forbes 30 Under 30 for his work.
+          {description}
         </div>
       </div>
     </div>
